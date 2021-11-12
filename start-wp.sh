12aad1590af6ea992cae75899537ccfabaf8a8b5
PedroRossa98 <pedro.rossa@tecnico.ulisboa.pt>
2021-11-12 13:37:51 +0000

    Update of .sh video

--- start-wp.sh
+++ start-wp.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 video_server=elab1.ist.utl.pt
-video_port=8005
-usb_camera=/dev/video15
+video_port=8006
+usb_camera=/dev/video0
+video_width=1280
+video_height=720
+video_frame=30/1
 
 killall python3
 killall gst-launch-1.0
 python3 main.py > /dev/null 2>&1 &
-gst-launch-1.0 v4l2src device=$usb_camera ! video/x-raw,width=640,height=480,framerate=10/1 ! clockoverlay time-format=\"%x - %X\" ! videoconvert ! x264enc ! h264parse ! rtph264pay config-interval=1 pt=96 ! udpsink host=$video_server port=$video_port async=false > /dev/null 2>&1 &
+gst-launch-1.0 v4l2src device=$usb_camera ! video/x-raw,width=$video_width,height=$video_height,framerate=$video_frame ! clockoverlay time-format="%x - %X" ! videoconvert ! omxh264enc ! h264parse ! rtph264pay config-interval=1 pt=96 ! udpsink host=$video_server port=$video_port async=false > /dev/null 2>&1 &
