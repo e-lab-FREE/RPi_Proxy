a12d5b5d9aada8fbfa5ec595b1e2dbd9405c5f67
PedroRossa98 <pedro.rossa@tecnico.ulisboa.pt>
2021-12-25 20:19:55 +0000

    Update

--- pic_interface/interface.py
+++ pic_interface/interface.py
@@ -56,12 +56,8 @@ def try_to_lock_experiment(config_json, serial_port):
         print("\-------- --------/\n")
     except:
         print("TODO: send error to server, pic is not conected")
-    try:
-        
-        match = re.search(r"^(IDS)(?P<exp_name>[^ \t]+)(?P<exp_state>[^ \t]+)$",pic_message)
-    except:
-        match = re.search(r"^(IDS)\s(?P<exp_name>[^ \t]+)\s(?P<exp_state>[^ \t]+)$",pic_message)
-        print("ERROR: on search!")
+
+    match = re.search(r"^(IDS)\s(?P<exp_name>[^ \t]+)\s(?P<exp_state>[^ \t]+)$",pic_message)
     print(config_json['id'])
     print(match.group("exp_name"))
     if match.group("exp_name") == config_json['id']:
