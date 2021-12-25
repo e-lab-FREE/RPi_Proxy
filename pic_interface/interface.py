403c7da9437fd1263f035a8a90d86140a37850e3
PedroRossa98 <pedro.rossa@tecnico.ulisboa.pt>
2021-12-25 21:11:52 +0000

    Update

--- pic_interface/interface.py
+++ pic_interface/interface.py
@@ -204,7 +204,7 @@ def do_stop() :
             pass
         if "STPOK" in pic_message.decode(encoding='ascii') :
             return True
-        elif  pic_message.decode(encoding='ascii').split("\t")[2] in ["CONFIGURED\n","RESETED\n"]  :
+        elif  pic_message.decode(encoding='ascii').split("\t")[2][0:-2] in ["CONFIGURED","RESETED"]  :
             print("aqui mesmo aqui")
             serial_port.flush()
             serial_port.reset_input_buffer()
