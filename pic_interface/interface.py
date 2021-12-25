fca405863163b98bcdde90b76961afc198798d71
PedroRossa98 <pedro.rossa@tecnico.ulisboa.pt>
2021-12-25 21:26:53 +0000

    Update

--- pic_interface/interface.py
+++ pic_interface/interface.py
@@ -196,13 +196,13 @@ def do_stop() :
             print("MENSAGEM DO PIC A CONFIRMAR STPOK:\n")
             print(pic_message.decode(encoding='ascii'))
             print("\-------- ! --------/\n")
-            # print ( pic_message.decode(encoding='ascii').split("\t")[2] in ["CONFIGURED","RESETED\r"])
+            print ( pic_message.decode(encoding='ascii').split("\t"))
         except:
             pass
         if "STPOK" in pic_message.decode(encoding='ascii') :
             return True
         elif "STP" in pic_message.decode(encoding='ascii') :
-            pas
+            pass
         elif pic_message.decode(encoding='ascii').split("\t")[2] != None and  pic_message.decode(encoding='ascii').split("\t")[2] in ["CONFIGURED\r","RESETED\r"] :
             print("aqui mesmo aqui")
             serial_port.reset_input_buffer()
