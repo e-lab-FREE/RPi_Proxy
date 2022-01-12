c76abfd710ee18661eb0afe6944f303a1cbf9bc1
PedroRossa98 <pedro.rossa@tecnico.ulisboa.pt>
2022-01-12 17:21:39 +0000

    Update

--- pic_interface/experiment_details.py
+++ pic_interface/experiment_details.py
@@ -18,6 +18,6 @@ def msg_to_config_experiment(config_json):
 
 def data_to_json(pic_message):
     return {"time":str(datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")[:-3]),\
-            "adc_value1":str(0.132088*float(pic_message[0]) - 66.383),\
-            "adc_value2":str(0.327324 * float (pic_message[1]) - 36),\
+            "adc_value1":str(float(0.132088*int(pic_message[0])) - 66.383),\
+            "adc_value2":str(float(0.327324 * int (pic_message[1])) - 36),\
             "adc_value3":str(pic_message[2])}
