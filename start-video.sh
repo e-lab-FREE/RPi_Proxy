e8aea50c706b7144cdfa208b95b66a132a78cf09
PedroRossa98 <64550870+PedroRossa98@users.noreply.github.com>
2022-04-08 18:54:46 +0100

    Update start-video.sh
    Update start-video.sh, using now the ffmpeg

--- start-video.sh
+++ start-video.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
-video_server=elab1.ist.utl.pt
-video_port=8006
-usb_camera=/dev/video0
-video_width=1280
-video_height=720
-video_frame=30/1
+pendulum_location=Lisbon
+pendulum_name="pendulo 12"
+video_server=127.0.0.1
+video_port=10000
+usb_camera=/dev/video2
+video_width=800
+video_height=600
+#video_frame=30/1
+video_frame=30
 
-killall gst-launch-1.0
-gst-launch-1.0 v4l2src device=$usb_camera ! video/x-raw,width=$video_width,height=$video_height,framerate=$video_frame ! clockoverlay time-format="%x - %X" ! videoconvert ! omxh264enc ! h264parse ! rtph264pay config-interval=1 pt=96 ! udpsink host=$video_server port=$video_port async=false 
+killall ffmpeg
+ffmpeg -s $video_width\x$video_height -f video4linux2 -i $usb_camera -r $video_frame \
+       -vf "drawtext = fontfile = /usr/share/fonts/truetype/ttf-dejavu/DejaVuSans-Bold.ttf: text ='$pendulum_location ($pendulum_name) - %{localtime\:%X}': :fontsize=20:fontcolor=black:box=1:boxcolor=white@0.5: x = 10: y = 10 " \
+       -pix_fmt yuv420p -an -c:v libx264 -profile:v baseline  -tune zerolatency -flags global_header -bsf dump_extra \
+       -f rtp "rtp://$video_server:$video_port/?pkt_size=1000"
