f18167821f9792cdc3266d156eaa36010bb418b5
PedroRossa98 <64550870+PedroRossa98@users.noreply.github.com>
2022-05-19 18:14:06 +0100

    Update video-stream.sh

--- video-stream.sh
+++ video-stream.sh
@@ -10,7 +10,7 @@ then
 
       ffmpeg -s $video_width\x$video_height -f video4linux2 -i $usb_camera -r $video_frame \
           -vf "drawtext = fontfile = /usr/share/fonts/truetype/ttf-dejavu/DejaVuSans-Bold.ttf: text ='$apparatus_location ($apparatus_name $apparatus_id) - %{localtime\:%X}': :fontsize=$font_size:fontcolor=black:box=1:boxcolor=white@0.5: x = 10: y = 10 " \
-          -pix_fmt yuv420p -an -c:v libx264 -profile:v baseline  -tune zerolatency -flags global_header -bsf dump_extra \
+          -pix_fmt yuv420p -an -c:v h264_omx -b:v 2048k -profile:v baseline  -tune zerolatency -flags global_header -bsf dump_extra \
           -f rtp "rtp://$video_server:$video_port/?pkt_size=1000"
 
 else
