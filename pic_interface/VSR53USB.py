86015a500dcadb90fdcb6d300179b29aa39ba1dc
PedroRossa98 <pedro.rossa@tecnico.ulisboa.pt>
2023-07-04 00:30:50 +0100

    update

--- pic_interface/VSR53USB.py
+++ pic_interface/VSR53USB.py
@@ -41,7 +41,7 @@ class VSR53USB:
         return  
 
     def int_VSR53USB(self,COM,Timeout):
-        serial_VSR53USB = serial.Serial(COM, timeout=Timeout,baudrate=115200)
+        serial_VSR53USB = serial.Serial(COM, timeout=Timeout,baudrate=9600)
         return serial_VSR53USB
        
 
